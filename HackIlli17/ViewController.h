14cd3e2ace611ab97de16546dc300feb1428f088
Jonathan Chan <jonchanyc@outlook.com>
2017-02-25 10:01:39 -0600

    Project structure

---
+++ HackIlli17/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  HackIlli17
+//
+//  Created by Jonathan Chan on 2017-02-25.
+//  Copyright © 2017 Jonathan Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Vuforia/Vuforia.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
