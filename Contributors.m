cdf2010f7066600094a62644dbaa9d91d73db90c
kekamui <kevinmui2020@u.northwestern.edu>
2017-02-26 05:59:48 -0600

    Create Contributors.m

---
+++ Contributors.m
@@ -0,0 +1,3 @@
+Jon:Implemented Vuforia in the beginning, implemented AR images, compiled the app together
+Youngeon:Implemented front-end design, created app-icon, app-design, and target images, implemented image recognition
+Kevin: Implemented target images and AR images and its databases into Vuforia, object-recognition
