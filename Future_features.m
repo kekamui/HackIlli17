48af2fab760dba94e09ca6c74a1872a44e88c605
kekamui <kevinmui2020@u.northwestern.edu>
2017-02-26 05:52:58 -0600

    Create Future_features.m

---
+++ Future_features.m
@@ -0,0 +1,2 @@
+Future features that we would like to implement include specialized text-based messages for different types of target images. For example, a desk could have a message/reminder about studying, and a window could give a message about the weather. In addition, we were thinking of enabling the app to show multiple messages at once, when more than one target image is on-screen. 
+	Current bugs include not being able to display specialized messages for different types of target images, and not being able to run on Android. These should be fixed in the future.
